49ab7b64bd62610f9b15f719cc8f792ccfc80523
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2025-10-08 22:45:32 +0700

    deploy

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Home-production.jsx';
+import QueueSystem from './src/QueueSystem.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueueSystem />
   </React.StrictMode>
 );
